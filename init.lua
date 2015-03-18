9861fda06b8b88c91a2d47a294c6b67166771f47
Ryan Erickson <ryan@ericksonfamily.com>
2015-03-17 22:51:30 -0600

    Added auto-configure code... not all working yet.

--- init.lua
+++ init.lua
@@ -5,33 +5,69 @@ gPINS = {R = 7, G = 4, B = 6, W = 5}
 PWMFREQHZ = 1000
 PWMDUTY = 512 -- ?
 
-local g, o = gpio, gpio.OUTPUT
+local _g, _o, _sd = gpio, gpio.OUTPUT, pwm.setduty
 
-for _, pin in pairs(gPINS) do
- g.mode(pin, o)
+for _, pin in pairs({4,5,6,7}) do
+ _g.mode(pin, _o)
  pwm.setup(pin, PWMFREQHZ, PWMDUTY)
  pwm.start(pin)
- pwm.setduty(pin, 0)
 end
 
 -- 0-1023 values
 function rgbw(r, g, b, w)
   r, g, b, w = r or 0, g or 0, b or 0, w or 0
-  pwm.setduty(gPINS.R, r)
-  pwm.setduty(gPINS.G, g)
-  pwm.setduty(gPINS.B, b)
-  pwm.setduty(gPINS.W, w)
+  _sd(gPINS.R, r)
+  _sd(gPINS.G, g)
+  _sd(gPINS.B, b)
+  _sd(gPINS.W, w)
 end
 
 rgbw()
 
-wifi.setmode(wifi.STATION)
-print('set mode=STATION (mode='..wifi.getmode()..')')
-print('MAC: ',wifi.sta.getmac())
-print('chip: ',node.chipid())
-print('heap: ',node.heap())
--- wifi config start
-wifi.sta.config("***REMOVED***","***REMOVED***")
--- wifi config end
-print("wifi config done.")
+-- Rewriting config.lua... to use in auto-config script...
+function save_config(ssid, pass)
+  ssid, pass = ssid or '', pass or ''
+  file.open("config.lua", "w")
+  file.close()
+  file.remove("config.lua")
+  file.open("config.lua", "w+")
+  file.writeline('CONFIG = {}')
+  if (ssid ~= '') then file.writeline('CONFIG.SSID = "' .. ssid .. '"') end
+  if (pass ~= '') then file.writeline('CONFIG.PASS = "' .. pass .. '"') end
+  file.flush()
+  file.close()
+end
 
+
+function prl(f, t)
+  print('loading ' .. f .. '.lua in ' .. t .. ' seconds')
+  tmr.alarm(0, t * 1000, 1, mkf(f, t))
+end
+
+function mkf(f, t)
+  return function() 
+            print('loading ' .. f ..'.lua...') 
+            tmr.stop(0)
+            pcall(dofile, f .. '.lua') end
+end
+
+-- wifi config start... Only do if we already have it... If not, do 'auto-discovery'
+-- load 'config.lua' file...
+pcall(dofile, "config.lua")
+CONFIG = CONFIG or {}
+
+if (CONFIG.SSID == nil) or (CONFIG.PASS == nil) then
+  prl('connect', 15)
+else
+  wifi.setmode(wifi.STATION)
+  wifi.sta.config(CONFIG.SSID, CONFIG.PASS)
+
+  print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  print('MODE: '..wifi.getmode())
+  print('MAC: ',wifi.sta.getmac())
+  print('chip: ',node.chipid())
+  print('heap: ',node.heap())
+  print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
+  prl('main', 15)
+end
--- main.lua
+++ main.lua
@@ -1,22 +1,29 @@
+-- Declare functions
+function main()
+  print("Main function...")
+  -- Startup webserver / UDP server???
+  -- Declare LCD 'fancy color scroll, etc.'
+end
+
 -- Print immediately IP if present, otherwise wait for it, print when done.
+local ret, err = pcall(function() 
+trycount = 20
 if (wifi.sta.getip() == nil) then
-  tmr.alarm(0, 1000, 1, function()
-     if wifi.sta.getip() == nil then
-        print("Connecting to AP...")
-     else
+  print("Waiting on AP [" .. (CONFIG.SSID or 'none') .. "]...")
+  tmr.alarm(0, 250, 1, function()
+     trycount = trycount - 1
+     if (trycount < 1) then tmr.stop(0) print("AP Not Found.") end
+     if wifi.sta.getip() ~= nil then
         print('IP: ',wifi.sta.getip())
         tmr.stop(0)
      end
   end)
 else
   print('IP: ',wifi.sta.getip())
-  main()
+  print('Starting main in 5 seconds...')
+  tmr.alarm(0, 5000, 1, function() tmr.stop(0) main() end)
 end
+end)
 
--- Declare functions
-function main()
-  -- Startup webserver / UDP server???
-  -- Declare LCD 'fancy color scroll, etc.'
-end
-
+if (not ret) then print("Error: " .. err) end
 
